dc4752cb963336e3b99e1525f0bb641eb467113c
kkyukiwork@gmail.com <kkyukiwork@gmail.com>
2022-06-15 18:27:39 +0500

    QueryVM

---
+++ TaskTracker_LOGIC/Services/ViewModels/Project/ProjectQuery/GetProjectQueryVM.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskTracker_DAL.Enums;
+
+namespace TaskTracker_LOGIC.Services.ViewModels.Project.ProjectQuery
+{
+    public class GetProjectQueryVM
+    {
+        public string? Name { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? CompletionDate { get; set; }
+        public ProjectPriority? Priority { get; set; }
+    }
+}
